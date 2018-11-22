40c7403a62f0275a4da2ec1eeae459bcbe19ed58
Automage <pranav92000@gmail.com>
2018-11-21 19:08:20 -0500

    Added getters and setters to Player class

--- src/models/Player.java
+++ src/models/Player.java
@@ -16,4 +16,20 @@ public class Player {
         this.name = name;
         this.playerSprites = playerSprites;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
