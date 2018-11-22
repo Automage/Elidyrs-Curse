3cae9328f3a7951ee0c4ef877a44fa7bc90e3263
Automage <pranav92000@gmail.com>
2018-11-21 19:07:22 -0500

    Added Player class

---
+++ src/models/Player.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Player {
+
+    private int x, y;
+    private final String name;
+    private Sprite[] playerSprites; //Sprites for left, right, walking, running, attacking, etc.
+
+    //Future framework
+    private double experience;
+    private int level; //level 3, 50, etc.
+
+    public Player(int x, int y, String name, Sprite... playerSprites) {
+        this.x = 0;
+        this.y = 0;
+        this.name = name;
+        this.playerSprites = playerSprites;
+    }
+}
