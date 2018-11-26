f9d9f542dc04e4a69e98b5f26ede7aa73ba57d19
Automage <pranav92000@gmail.com>
2018-11-26 02:37:37 -0500

    inverted movement fixed

--- src/engine/GameEngine.java
+++ src/engine/GameEngine.java
@@ -29,21 +29,22 @@ public class GameEngine {
 
     public void update() {
         //Player updates
+        //Coordinate system starts at top-left - so y-axis is inverted
         //UP
         if (keyboard.keyIsDown[0]) {
-            player.move(0, player.getSpeed());
+            player.move(0, -player.getSpeed());
         }
         //DOWN
         if (keyboard.keyIsDown[1]) {
-            player.move(0, -player.getSpeed());
+            player.move(0, player.getSpeed());
         }
         //LEFT
         if (keyboard.keyIsDown[2]) {
-            player.move(player.getSpeed(), 0);
+            player.move(-player.getSpeed(), 0);
         }
         //RIGHT
         if (keyboard.keyIsDown[3]) {
-            player.move(-player.getSpeed(), 0);
+            player.move(player.getSpeed(), 0);
         }
         player.update();
         System.out.println("x: " + player.getX() + " y: " + player.getY());
